0b28517792d7fe196a2fe87d18d6ab8a87d6f22d
Talha Hasan Zia <talhahasanzia@hotmail.com>
2017-11-30 14:47:37 +0500

    added comments

--- SwipeView/app/src/main/java/com/example/me/swipeview/ListViewAdapter.java
+++ SwipeView/app/src/main/java/com/example/me/swipeview/ListViewAdapter.java
@@ -70,79 +70,87 @@ public class ListViewAdapter extends ArrayAdapter {
 
         textView.setText("item " + (position + 1));
 
-
+        // this is where the real magic happens
         textView.setOnTouchListener(new View.OnTouchListener() {
 
+            // to register distance between View's X and screen touch's X coordinate
             float dX;
 
-            int lastAction;
-            float startingMove;
+            int lastAction; // last action to be tracked
+            float startingMove; // starting X coodinates
 
 
             @Override
             public boolean onTouch(View view, MotionEvent event) {
 
+                // assuming that the view to be swiped has full screen width
+                // initialize width if it is not initialized yet
                 if (mWidth == 0) {
                     mWidth = view.getMeasuredWidth();
                     midPoint = mWidth / 2.0f;
                 }
 
+                // this block saves width of the buttons/views where top view will settle after swipe
                 if (measuredFromLeft == 0) {
 
 
-                    measuredFromRight = remove.getMeasuredWidth();
-                    measuredFromLeft = measuredFromRight * 2;
+                    measuredFromRight = remove.getMeasuredWidth(); // it will settle from a distance of 1 view unit from right
+                    measuredFromLeft = measuredFromRight * 2;       // it will settle from a distance of 2 view unit from right
 
                 }
                 //Log.d(TAG, "onTouch: " + event.getActionMasked());
                 switch (event.getActionMasked()) {
                     case MotionEvent.ACTION_DOWN:
-                        dX = view.getX() - event.getRawX();
-                        //dY = view.getY() - event.getRawY();
-                        lastAction = MotionEvent.ACTION_DOWN;
-                        startingMove = view.getX();
+                        dX = view.getX() - event.getRawX(); // calculate distance between pointer touch and view initials
+                        // this is necessary other wise view will jump to whereeven the screen is Tapped
+
+                        lastAction = MotionEvent.ACTION_DOWN; // set last action for future checks (in ACTION_UP)
+                        startingMove = view.getX(); // starting X coordinates
                         break;
 
                     case MotionEvent.ACTION_MOVE:
-                        // view.setY(event.getRawY() + dY);
-                        view.setX(event.getRawX() + dX);
-                        lastAction = MotionEvent.ACTION_MOVE;
+                        // while user drags move the view along
+                        view.setX(event.getRawX() + dX); // with difference in views initial position to be covered
+                        lastAction = MotionEvent.ACTION_MOVE; // update last action
                         break;
 
-                    case MotionEvent.ACTION_CANCEL:
+                    case MotionEvent.ACTION_CANCEL: // in case the pointer (Down) is canceled
                     case MotionEvent.ACTION_UP: {
+                        // if this was after a drag, calculate position and states
                         if (lastAction == MotionEvent.ACTION_MOVE) {
 
 
-                            Log.d(TAG, "onTouch: X-axis of view: " + view.getX());
-                            if (view.getX() - startingMove > 0) {
+                          //  Log.d(TAG, "onTouch: X-axis of view: " + view.getX());
+                            if (view.getX() - startingMove > 0) {  // RIGHT SWIPE
                                 // Log.d(TAG, "onTouch: swiped right");
 
+
+                                // if swipe is as long as it crossed half the screen length
                                 if (view.getX() > midPoint) {
                                     Toast.makeText(context, "Full Swipe to Right", Toast.LENGTH_SHORT).show();
                                     restore(view);
-                                } else if (CURRENT_STATE == RIGHT_OPTIONS_OPENED) {
-                                    restore(view);
+                                } else if (CURRENT_STATE == RIGHT_OPTIONS_OPENED) { // if right views were visible
+                                    restore(view); // they should be hidden
                                 } else {
-                                    toLeftOptions(view);
+                                    toLeftOptions(view); // no views were visible so right swipe should show left options
                                 }
                             } else {
 
-
+                                // if not right swipe then left swipe  (duh)
                                 //Log.d(TAG, "onTouch: swiped left");
-                                if (view.getX() * -1 > midPoint) {
+                                if (view.getX() * -1 > midPoint) { // check if swipe passed mid screen as full swipe
                                     Toast.makeText(context, "Full Swipe to Left", Toast.LENGTH_SHORT).show();
 
 
                                 } else if (CURRENT_STATE == LEFT_OPTIONS_OPENED) {
-
+                                    // if left options (views) are visible restore the whole view
                                     restore(view);
-                                } else {
+                                } else { // if the list item was in initial position then show right options on left swipe
                                     toRightOptions(view);
                                 }
                             }
 
-
+                        // NOTE THAT ONLY ONE SIDE OPTIONS ARE VISIBLE AT ONCE
                         }
                     }
                     break;
